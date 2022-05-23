b7f7afc6e1835e0e3fef5a3bbfa68436d6b5e903
alesparza <aesparza@seas.upenn.edu>
2022-05-23 09:38:33 -0400

    feat: my_program.c for scenario-0

---
+++ my_program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void printHello() {
+    printf("Hello World\n");
+}
+
+void printString(char* string) {
+    printf("argument: %s\n", string);
+    printf("first char of argument: %c\n", string[0]);
+}
+
+int main(int argc, char** argv) {
+    printf("argc = %d\n", argc);
+    printf("argv[0] = %s\n", argv[0]);
+    printHello();
+    printString(argv[1]);
+    
+}
+
